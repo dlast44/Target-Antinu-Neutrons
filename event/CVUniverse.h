7626454ca975f03d8b2590538ed04e2132d685db
David Last <lastd44@gmail.com>
2021-09-13 14:39:00 -0500

    Merged in functions, etc. from old CVUniverse, except for Neutron Candidate Objects. Builds/runs without breaking anything.

--- event/CVUniverse.h
+++ event/CVUniverse.h
@@ -1,4 +1,7 @@
 // =============================================================================
+// David Last adaptation of the CVUniverse for his analysis.
+// Contact email: dlast@sas.upenn.edu (lastd44@gmail.com)
+//
 // Base class for an un-systematically shifted (i.e. CV) universe. Implement
 // "Get" functions for all the quantities that you need for your analysis.
 //
@@ -16,13 +19,19 @@
 
 #include <iostream>
 
+#include "PlotUtils/PhysicsVariables.h"//Included by David, unsure if needed
 #include "PlotUtils/MinervaUniverse.h"
+
+//Includes for old neutron candidate structure which is not currently added into this structure.
+//#include "obj/NeutCands.h"
+//#include "TVector3.h"
 
 class CVUniverse : public PlotUtils::MinervaUniverse {
 
   public:
   #include "PlotUtils/MuonFunctions.h" // GetMinosEfficiencyWeight
   #include "PlotUtils/TruthFunctions.h" //Getq3True
+  #include "PlotUtils/RecoilEnergyFunctions.h" //GetRecoilEnergy
   // ========================================================================
   // Constructor/Destructor
   // ========================================================================
@@ -187,9 +196,161 @@ class CVUniverse : public PlotUtils::MinervaUniverse {
     return TMath::Sqrt(pow(nuclMass,2) + 2.0*(Enu-Emu)*nuclMass - Q2);
   }
 
+  // Functions added by David that have no match in the above.
+  virtual int GetNTracks() const { return GetInt("multiplicity"); }
+
+  virtual int GetNNeutBlobs() const { return GetInt("MasterAnaDev_BlobIs3D_sz"); }
+
+  virtual std::vector<double> GetEMBlobStartZVec() const { return GetVec<double>("nonvtx_iso_blobs_start_position_z_in_prong"); }
+
+  virtual std::vector<int> GetEMBlobNHitsVec() const { return GetVec<int>("nonvtx_iso_blobs_n_hits_in_prong"); }
+
+  virtual std::vector<double> GetEMBlobEnergyVec() const { return GetVec<double>("nonvtx_iso_blobs_energy_in_prong"); }
+
+  virtual std::vector<double> GetEMNBlobsTotalEnergyTotalNHits(double shift = 0) const {
+    std::vector<double> info;
+    double nBlobs = 0;
+    double totalE = shift;
+    double nHits = 0;
+    std::vector<double> StartZVec = GetEMBlobStartZVec();
+    std::vector<double> EnergyVec = GetEMBlobEnergyVec();
+    std::vector<int> NHitsVec = GetEMBlobNHitsVec();
+    for (unsigned int i=0; i<StartZVec.size(); ++i){
+      if (StartZVec.at(i) > 4750.0){
+	nBlobs+=1.0;
+	totalE+=EnergyVec.at(i);
+	nHits+=(double)NHitsVec.at(i);
+      }
+    }
+    info.push_back(nBlobs);
+    info.push_back(totalE);
+    info.push_back(nHits);
+    return info;
+  }
+
+  virtual int GetTargetZ() const { return GetInt("mc_targetZ"); }
+
+  virtual int GetNFSPart() const { return GetInt("mc_nFSPart"); }
+
+  virtual std::vector<int> GetFSPartPDG() const { return GetVec<int>("mc_FSPartPDG"); }
+
+  virtual std::vector<double> GetFSPartE() const { return GetVec<double>("mc_FSPartE"); }
+
+  virtual std::vector<double> GetFSPartPx() const { return GetVec<double>("mc_FSPartPx"); }
+
+  virtual std::vector<double> GetFSPartPy() const { return GetVec<double>("mc_FSPartPy"); }
+
+  virtual std::vector<double> GetFSPartPz() const { return GetVec<double>("mc_FSPartPz"); }
+
+  virtual int GetNImprovedMichel() const { return GetInt("improved_michel_vertex_type_sz"); }
+
+  virtual int GetNuHelicity() const { return GetInt("MasterAnaDev_nuHelicity"); }
+
+  virtual double GetEnuCCQEPickledGeV() const{ //RETURNS IN MeV^2
+    int charge=-1; //hard-coded since I'm focused on anti-nu
+    double enu=PlotUtils::nuEnergyCCQE( GetEmu(), GetPmu(), GetThetamu(), charge)*MeVGeV;
+    return enu;
+  }
+
+  virtual double GetQ2QEPickledGeV() const{ //RETURNS IN MeV^2
+    int charge=-1; //hard-coded since I'm focused on anti-nu
+    if (GetEnuCCQEPickledGeV()<=0.0) return 0.0;
+    else{
+      double q2=PlotUtils::qSquaredCCQE( GetEmu(), GetPmu(), GetThetamu(), charge)*MeVGeV*MeVGeV;
+      return q2;
+    }
+  }
+
+  // Functions added by David that have a confusing match to above, be careful with naming
+  virtual int GetHasInteractionVertex() const { return GetInt("has_interaction_vertex"); }
+
+  virtual std::vector<double> GetVtx() const { return GetVec<double>("vtx"); }
+
+  virtual int GetIsMinosMatchTrack() const { return GetInt("isMinosMatchTrack"); }
+  
+  virtual int GetIsMinosMatchTrackOLD() const { return GetInt("muon_is_minos_match_track"); }
+  
+  virtual int GetIsMinosMatchStub() const { return GetInt("isMinosMatchStub"); }
+  
+  virtual int GetIsMinosMatchStubOLD() const { return GetInt("muon_is_minos_match_stub"); }
+
+  // Functions added by David that have a conflicting match above, be careful with naming
+  double MeVGeV=0.001;
+
+  virtual double GetCalRecoilEnergy() const{
+    if (GetVec<double>("recoil_summed_energy").size()==0) return -999.0;
+    return (GetVec<double>("recoil_summed_energy")[0]-GetDouble("recoil_energy_nonmuon_vtx100mm"));
+  }
+
+  virtual double GetNonCalRecoilEnergy() const{
+    return 0.0;
+  }
+
+  virtual double GetDANRecoilEnergyGeV() const{
+    double recoilE = GetDouble("recoil_energy_nonmuon_nonvtx100mm")*MeVGeV;
+    return recoilE;
+  }
+
+  virtual double GetRecoilEnergyGeV() const{
+    double recoilE = GetRecoilEnergy()*MeVGeV;
+    return recoilE;
+  }
+
   //Still needed for some systematics to compile, but shouldn't be used for reweighting anymore.
   protected:
   #include "PlotUtils/WeightFunctions.h" // Get*Weight
 };
+
+  //Neutron Candidate Business which may be included at some point
+  /*
+
+  virtual NeutronCandidates::NeutCand GetNeutCand(int index){
+    std::vector<double> vtx = GetVtx();
+    TVector3 EvtVtx;
+    EvtVtx.SetXYZ(vtx.at(0),vtx.at(1),vtx.at(2));
+    NeutronCandidates::intCandData intData;
+    NeutronCandidates::doubleCandData doubleData;
+    for (const auto& intMember: NeutronCandidates::GetBranchIntMap()){
+      intData[intMember.first]={};
+      for (const auto& branchName: intMember.second){
+	intData[intMember.first].push_back(GetVecElemInt(branchName,index));
+      }
+    }
+    for (const auto& doubleMember: NeutronCandidates::GetBranchDoubleMap()){
+      doubleData[doubleMember.first]={};
+      for (const auto& branchName: doubleMember.second){
+	doubleData[doubleMember.first].push_back(GetVecElem(branchName,index));
+      }
+    }
+    return NeutronCandidates::NeutCand(intData,doubleData,EvtVtx);
+  };
+  
+  virtual NeutronCandidates::NeutCands GetNeutCands(){
+    std::vector<NeutronCandidates::NeutCand> cands;
+    int nBlobs = GetNNeutBlobs();
+    for(int neutBlobIndex=0; neutBlobIndex < nBlobs; ++neutBlobIndex){
+      cands.push_back(GetNeutCand(neutBlobIndex));
+    }
+    NeutronCandidates::NeutCands EvtCands(cands);
+    return EvtCands;
+  };
+
+  virtual void UpdateNeutCands(){
+    NeutronCandidates::NeutCands candsIn = GetNeutCands();
+    fNeutCands = candsIn;
+    fNNeutCands = candsIn.GetNCands();
+  };
+
+  NeutronCandidates::NeutCand GetCurrentNeutCand(int index) { return fNeutCands.GetCandidate(index); };
+
+  NeutronCandidates::NeutCand GetCurrentLeadingNeutCand() { return fNeutCands.GetMaxCandidate(); };
+
+  NeutronCandidates::NeutCands GetCurrentNeutCands() { return fNeutCands; };
+
+  int GetNNeutCands(){ return fNNeutCands; }
+  
+ private:
+  NeutronCandidates::NeutCands fNeutCands;
+  int fNNeutCands;*/
 
 #endif
